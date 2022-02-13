ab48318425bfeb5a7038180b1c559c63e9d2fc9c
ShadKhanFF <desinershadkhan@gmail.com>
2022-02-13 17:39:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import bg from './bg.jpg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+    <img src={bg} />
+    </div>
+    <h1>Shad Khan</h1>
+    <p>I am Shad Khan and I am a Web Developer. <br/>I work primarily with HTML, CSS, JavaScript and React Js.</p>
+    <h4>Links</h4>
+    <hr/>
+    <a href="https://www.google.com/gmail/" target="_blank"><i class="fa-solid fa-envelope"></i></a>
+    <a href="https://github.com/shadkhanff" target="_blank"><i class="fa-brands fa-github"></i></a>
+    <a href="https://facebook.com/" target="_blank"><i class="fa-brands fa-facebook"></i></a>
+    <a href="https://www.facebook.com/messages/" target="_blank"><i class="fa-brands fa-facebook-messenger"></i></a>
+    <a href="http://www.shadkhan.xyz/" target="_blank"><i class="fa-brands fa-sitrox"></i></a>
     </div>
   );
 }
